a0113da8c7aeda3206beea151c0dbdd7c59c34b6
Yaz Khoury <yaz.khoury@gmail.com>
2018-01-29 08:20:18 -0500

    feat: extend server config

--- webserver.py
+++ webserver.py
@@ -2,9 +2,13 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 def main():
     try:
-        pass
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print("Web Server running on port {}".format(port))
+        server.serve_forever()
     except KeyboardInterrupt:
-        pass
+        print("^C Entered, stopping web server...")
+        server.socket.close()
 
 if __name__ == "__main__":
     main()
