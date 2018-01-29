e41ce30847b392cdf99cfa995c18e3ab6fb770ae
Yaz Khoury <yaz.khoury@gmail.com>
2018-01-29 11:08:09 -0500

    feat: add get method

--- webserver.py
+++ webserver.py
@@ -1,9 +1,30 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
+class WebServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response("200")
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>Hello!</body></html>"
+                self.wfile.write(output)
+                print(output)
+                return
+        except IOError:
+            self.send_error(404, "File Not Found {}".format(self.path))
+
+    def do_POST(self):
+        try:
+            pass
+        except:
+            pass
+
 def main():
     try:
         port = 8080
-        server = HTTPServer(('', port), webserverHandler)
+        server = HTTPServer(('', port), WebServerHandler)
         print("Web Server running on port {}".format(port))
         server.serve_forever()
     except KeyboardInterrupt:
