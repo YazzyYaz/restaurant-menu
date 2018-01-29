44259060b016e40989f57b89d787988e0c1ba287
Yaz Khoury <yaz.khoury@gmail.com>
2018-01-29 08:16:31 -0500

    feat: add server barebone

---
+++ webserver.py
@@ -0,0 +1,10 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+def main():
+    try:
+        pass
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
