b3f2e4b43b1ed2c6cfa5cbca02009448c5f2126a
Yaz Khoury <yaz.khoury@gmail.com>
2018-01-28 23:04:49 -0500

    feat: add columns to tables

--- database_setup.py
+++ database_setup.py
@@ -8,8 +8,19 @@ Base = declarative_base()
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
